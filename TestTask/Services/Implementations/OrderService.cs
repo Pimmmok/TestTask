65379e2bc6d2065287dd9edd64c0e4d6bd55185c
Вадим Качан <pim_reffery@mail.ru>
2024-02-18 22:52:49 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        readonly ApplicationDbContext db;
+        public OrderService(ApplicationDbContext context)
+        {
+            db = context;
+        }
+
+        public Task<Order> GetOrder()
+        {
+            return db.Orders.FirstAsync(p => p.Price * p.Quantity == db.Orders.Max(p => p.Price * p.Quantity));
+        }
+
+        public Task<List<Order>> GetOrders()
+        {
+            return db.Orders.Where(p => p.Quantity > 10).ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        readonly private ApplicationDbContext db;
+        public UserService(ApplicationDbContext context)
+        {
+            db = context;
+        }
+        public Task<User> GetUser()
+        {
+            var maxCountOfOrders = db.Users.Include(c => c.Orders).Select(c => new { c.Id, c.Orders.Count }).
+        OrderBy(c => c.Count).Last();
+            return db.Users.FirstAsync(c => c.Id == maxCountOfOrders.Id);
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            return db.Users.Where(p => p.Status == UserStatus.Inactive).ToListAsync();
+        }
+    }
+}
